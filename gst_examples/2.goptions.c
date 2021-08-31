c3800ce016b11a99e27aa19168363f480128442d
Anupam Kaul <anupam.kaul@yahoo.com>
2021-08-31 16:27:44 -0700

    Add template for GOptions

---
+++ gst_examples/2.goptions.c
@@ -0,0 +1,11 @@
+// I can also use a GOption table to initialize my own parameters as shown in the code below
+
+#include <stdio.h>
+#include <gst/gst.h>
+
+int main(int argc, char *argv[])
+{
+
+    printf("Run me with --help to see the Application options appended. \n");
+    return 0;
+}
